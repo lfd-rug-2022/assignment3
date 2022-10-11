08e76e580194e3578298c5037682dc73d88dd9b3
Murali Manohar Kondragunta <kmanoharmurali@gmail.com>
2022-10-11 22:08:12 +0200

    README file and also on top of the script

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# assignment3
-LSTM baseline and Transformer system
+
+# Baseline LSTM
+
+Trains a LSTM model using train & dev set. If test set is mentioned, accuracy/F1-score (Macro)
+is calculated with an option of showing the confusion matrix.
+
+## Usage,
+
+*. One can simply run the file without any command line arguments because the script 
+is set up with best settings as default values for arguments.
+
+`python lfd_assignment3.py`
+
+Running with the best hyperparams must give a macro F1-score of around 89/90.
+
+Or if you want to change any of the arguments, please type
+
+`python lfd_assignment3.py --help`
--- lfd_assignment3.py
+++ lfd_assignment3.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
-'''TODO: add high-level description of this Python script'''
+''' Trains a LSTM model using train & dev set. If test set is mentioned, accuracy/F1-score (Macro)
+is calculated with an option of showing the confusion matrix.
+
+To run this file,
+
+1. One can simply run the file without any command line arguments because the script 
+is set up with best settings as default values for arguments.
+
+python lfd_assignment3.py
+
+Running with the best hyperparams must give a macro F1-score of around 89/90.
+
+Or if you want to change any of the arguments, please type
+
+python lfd_assignment3.py --help
+'''
 
 import random as python_random
 import json
